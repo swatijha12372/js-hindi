60b97fa88adb22ed3029af137f6a3845760b8f07
swatijha12372 <swatijha12372@gmail.com>
2026-08-07 08:27:14 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,30 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (primitive) , Heap (non-primitive)
+
+let myYoutubename = "swatijhadotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com" , 
+    upi: "user@ybl"
+}
+
+let userTwo = userOne  
+
+userTwo.email = "swatijha@gooogle.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
